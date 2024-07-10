a9c624f3b050cc90f3c43f98b3038b07acd9a956
Lucas Rocha de Oliveira Rodrigues <lucas.rodrigues@iter.com.br>
2024-07-10 14:13:35 -0300

    Leason 1

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
-import { useEffect, lazy, Suspense } from 'react';
-import { useDispatch } from 'react-redux';
-
-import { Routes, Route } from 'react-router-dom';
-
-import Spinner from './components/spinner/spinner.component';
-import { checkUserSession } from './store/user/user.action';
-import { GlobalStyle } from './global.styles';
-
-const Navigation = lazy(() =>
-  import('./routes/navigation/navigation.component')
-);
-const Shop = lazy(() => import('./routes/shop/shop.component'));
-const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
-const Home = lazy(() => import('./routes/home/home.component'));
-const Authentication = lazy(() =>
-  import('./routes/authentication/authentication.component')
-);
+import "./App.css";
 
 const App = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(checkUserSession());
-  }, []);
-
   return (
-    <Suspense fallback={<Spinner />}>
-      <GlobalStyle />
-      <Routes>
-        <Route path='/' element={<Navigation />}>
-          <Route index element={<Home />} />
-          <Route path='shop/*' element={<Shop />} />
-          <Route path='auth' element={<Authentication />} />
-          <Route path='checkout' element={<Checkout />} />
-        </Route>
-      </Routes>
-    </Suspense>
+    <div className="App">
+      <div className="categories-container">
+        <div className="category-container">
+          {/*img*/}
+          <div className="category-body-container"> 
+            <h2>Hats</h2>
+            <p>Shop now</p>
+          </div>
+        </div>
+        <div className="category-container">
+          {/*img*/}
+          <div className="category-body-container"> 
+            <h2>Jackets</h2>
+            <p>Shop now</p>
+          </div>
+        </div>
+        <div className="category-container">
+          {/*img*/}
+          <div className="category-body-container"> 
+            <h2>Sneakers</h2>
+            <p>Shop now</p>
+          </div>
+        </div>
+        <div className="category-container">
+          {/*img*/}
+          <div className="category-body-container"> 
+            <h2>Womans</h2>
+            <p>Shop now</p>
+          </div>
+        </div>
+        <div className="category-container">
+          {/*img*/}
+          <div className="category-body-container"> 
+            <h2>Mens</h2>
+            <p>Shop now</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,30 +1,17 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Elements } from '@stripe/react-stripe-js';
-
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import { store, persistor } from './store/store';
-import { stripePromise } from './utils/stripe/stripe.utils';
-import * as registerServiceWorker from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById('root');
-
-render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <Elements stripe={stripePromise}>
-            <App />
-          </Elements>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>,
-  rootElement
+    <App />
+  </React.StrictMode>
 );
 
-registerServiceWorker.register();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
